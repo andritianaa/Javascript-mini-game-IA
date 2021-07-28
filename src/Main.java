4e5671f46f9b05610028b507a1538212413739e5
Andritianaa <andritianasteve@gmail.com>
2021-07-28 22:59:20 +0300

    nampiana commentaire

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * tableau mitazona count ana matiere
+         * le tableau faharoa mitondra ny anarana matière
+         * tsy natao tableau 2D satria ny ray int de ny ray String
+         * raha hanaovana tri ny tableau ray de ny ray koahatao tri satria mifandray
+         **/
+        int[] listSerie = new int[4];
+        String[] listSerieName = {" ","Scientifique","Litéraire","Neutre"};
+        
+        /**scanner hanaovana saisie console
+          esorina rehefa miditra mobile**/
+        Scanner sc = new Scanner(System.in);
+        /**
+         * initialisena ilay liste ana matière
+         * ny contenu an'ny liste ary amin'ny class Liste
+         **/
+        Liste listeMatiere = new Liste();
+        Matiere[] liste = listeMatiere.getListeMatiere();
+        /**
+         * asesy tsirairay ny matière rehetra de raha tiana dia saisisena 1 ny valeur raha tsy tiana de 0
+         * tsy maintsy 0 na 1 ihany ny valeur ampidirina anaty tmp satria "na tiana ilay matière na tsy tiana
+         **/
+        for(int i = 0; i < liste.length; ++i) {
+            int tmp;
+            do{
+                System.out.println(liste[i].getNom()+" ");
+                tmp = sc.nextInt();
+                if (tmp == 1) liste[i].setValeur(true);
+            }while (tmp!=0 && tmp!=1);
+            
+            /**
+             * izay matière tiana incrémentena anakiray ny valeur an'ny série mifandraika aminazy
+             **/
+            if      (liste[i].getSerie() == 1 && liste[i].getValeur().equals(true)) listSerie[1]++;
+            else if (liste[i].getSerie() == 2 && liste[i].getValeur().equals(true)) listSerie[2]++;
+            else if (liste[i].getSerie() == 3 && liste[i].getValeur().equals(true)) listSerie[3]++;
+        }
+        
+        System.out.println("\n");
+        for(int i = 1; i < liste.length; ++i) {
+            System.out.println(liste[i].getNom()+" "+liste[i].getValeur());
+        }
+        System.out.println("\n");
+        for(int i = 1; i < listSerie.length; ++i) {
+            System.out.println(listSerieName[i]+" "+listSerie[i]);
+        }
+    }
+}
---
+++ src/Matiere.java
@@ -0,0 +1,35 @@
+public class Matiere {
+    private String nom;
+    private Boolean valeur;
+    private int serie;
+    
+    public Matiere(String pnom) {
+        this.nom = pnom;
+        this.valeur = false;
+    }
+    
+    public Matiere(String pnom, int pserie) {
+        this.nom = pnom;
+        this.serie = pserie;
+        this.valeur = false;
+    }
+    /**geter**/
+    
+    public int getSerie() {
+        return this.serie;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public Boolean getValeur() {
+        return this.valeur;
+    }
+    
+    /**seter**/
+
+    public void setValeur(boolean pvaleur) {
+        this.valeur = pvaleur;
+    }
+}
